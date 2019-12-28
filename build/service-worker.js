ca7df15d4d85e7e6ee0bd7e0c7cc9fc1f2ba7afb
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2019-12-28 15:59:27 +0300

    Initial Commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab76e93f443275aac1ce7c3cf8964b68.js"
+  "/Monsters-Rolodex/precache-manifest.1f3c7b7767d3e7fa8eaf470db15703bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Monsters-Rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
