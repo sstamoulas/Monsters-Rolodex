3893f6a1e5964afdd8e38ade28b9b3683fc4f185
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2019-12-28 15:57:05 +0300

    Updates

---
+++ precache-manifest.1f3c7b7767d3e7fa8eaf470db15703bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c265607928ebc7560a9686d188fe5fc0",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "d7b4d25d2deea7ad86ac",
+    "url": "/Monsters-Rolodex/static/css/main.aa435ce6.chunk.css"
+  },
+  {
+    "revision": "b09ee22a8e5249579ef9",
+    "url": "/Monsters-Rolodex/static/js/2.c1e8c6ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.c1e8c6ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b4d25d2deea7ad86ac",
+    "url": "/Monsters-Rolodex/static/js/main.a947044d.chunk.js"
+  },
+  {
+    "revision": "af771e88b14abdc287b8",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.bfbb36c3.js"
+  }
+]);
